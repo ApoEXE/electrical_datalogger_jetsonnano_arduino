2411400598871d1d28525791289391a96e411dae
javier pernia <javierpernia89@gmail.com>
2022-04-17 17:56:05 -0500

    apparently working oled display on diff thread 0.5 s gettin sample

--- app.py
+++ app.py
@@ -24,20 +24,25 @@ import signal
 
 import csv
 
-
+import pathlib
 
 is_shutdown = False
 realvolt = 4.59
 # DECLARATIONS
 var_current_ac= 0
 var_volt_ac = 0
-bus = smbus.SMBus(0)
+
 reset = 0
 index_data = int(0)
+path="/home/nano/projects/electrical_datalogger_jetsonnano_arduino/ac_telemetry.csv"
+
 
 print(f"START at {datetime.datetime.now()}")
+file = pathlib.Path(path)
+if(file.exists()==False):
+    file = open(path,"x")
 
-file =  open("/home/nano/projects/electrical_datalogger_jetsonnano_arduino/ac_telemetry.csv","r+")
+file =  open(path,"r+")
 ac_writer = csv.writer(file,delimiter=',',quoting=csv.QUOTE_MINIMAL)
 reader = csv.reader(file)
 data = list(reader)
@@ -50,7 +55,7 @@ app = Flask(__name__)
 
 #print(f"FIRST ROW: {string_tmp}  {ac_value}")
 
-
+bus = smbus.SMBus(0)
 
 # 128x64 display with hardware I2C:
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=None, i2c_bus=0, gpio=1) # 
@@ -68,33 +73,32 @@ bottom = height-padding
 x = 0
 font = ImageFont.load_default()
         # Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
+
 #-------------------------------DISPLAY END
 
 
 def oled_disp():
     global var_volt_ac, var_current_ac,bus
-    while not is_shutdown:
-
-
-        # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-        cmd = "ifconfig wlan0 | grep 'inet ' | cut -c 14-26"
-        IP = subprocess.check_output(cmd, shell = True )
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+    cmd = "ifconfig wlan0 | grep 'inet ' | cut -c 14-26"
+    IP = subprocess.check_output(cmd, shell = True )
             # Write two lines of text.
-        draw.text((x, top),       "IP: " + str(IP.decode('utf-8')),  font=font, fill=255)
-        draw.text((x, top+8),      "Amp: " +str(var_current_ac)+"A", font=font, fill=255)
-        draw.text((x, top+16),     "Volt: " +str(var_volt_ac)+"Vac",  font=font, fill=255)
-        # draw.text((x, top+24),    str(SwapUsage.decode('utf-8')),  font=font, fill=255)
-        # draw.text((x, top+32),    str(Disk.decode('utf-8')),  font=font, fill=255)
-        # draw.text((x, top+40),    str(Date.decode('utf-8')),  font=font, fill=255)
-        # Display image.
+    draw.text((x, top),       "IP: " + str(IP.decode('utf-8')),  font=font, fill=255)
+    draw.text((x, top+8),      "Amp: " +str(var_current_ac)+"A", font=font, fill=255)
+    draw.text((x, top+16),     "Volt: " +str(var_volt_ac)+"Vac",  font=font, fill=255)
+    # draw.text((x, top+24),    str(SwapUsage.decode('utf-8')),  font=font, fill=255)
+    # draw.text((x, top+32),    str(Disk.decode('utf-8')),  font=font, fill=255)
+    # draw.text((x, top+40),    str(Date.decode('utf-8')),  font=font, fill=255)
+    # Display image.
+    try:
         disp.image(image)
         disp.display()
-
+        
+    except:
+        print(f"ERROR display 0x3C i2c disconnection")
 
 def gather_data():
     global var_volt_ac, var_current_ac,bus
-
     address = 0x20
     try:
 
@@ -103,11 +107,11 @@ def gather_data():
         #print(read)
         #print('\n'.join('{}: {}'.format(*val) for val in enumerate(read)))
         ac_curr = read[0]<<8 | read[1]
-        #print('AC Current in ',end="")
+        print('AC Current in ',end="")
         ac_curr = ac_curr/1000.0
         var_current_ac = ac_curr
-        #print("%.3f" %ac_curr,end="")
-        #print('A  ',end="")
+        print("%.3f" %ac_curr,end="")
+        print('A  ',end="")
         ac_volt_dig = read[2]<<8 | read[3]
         #print(ac_volt_dig)
         anaVolt = ac_volt_dig*(realvolt / 1023.0)
@@ -117,15 +121,12 @@ def gather_data():
         v_string = str(volt_ac)
         volt_ac_string = v_string[0] +v_string[1] +v_string[2] +v_string[3] +v_string[4]+ v_string[5]+ v_string[6]
         var_volt_ac = volt_ac_string
-        #print(volt_ac_string,end="")
-        #print("%.3f" %volt_ac,end="")
-        #print('Vac')
+        print(volt_ac_string,end="")
+        print("%.3f" %volt_ac,end="")
+        print('Vac')
 
     except:
-        print(f"ERROR gather 0x20 i2c disconnection at {datetime.datetime.now()}")
-        print(f"ERROR gather 0x20 restarting at {datetime.datetime.now()}")
-    finally:
-        5
+        print(f"ERROR gather 0x20 i2c disconnection")
     today = date.today()
     d1 = today.strftime("%Y/%m/%d")
     from datetime import datetime
@@ -133,20 +134,27 @@ def gather_data():
     d2 = now.strftime("%H:%M:%S")
     ac_writer.writerow([d1,d2,volt_ac*ac_curr,volt_ac,ac_curr])
     file.flush()
-    time.sleep(0.1)
-    oled_disp()
-    time.sleep(0.1)
+    #time.sleep(0.1)
+    
+    #oled_disp()
+    #time.sleep(0.1)
     return [volt_ac,ac_curr]
 
 def gather_loop():
     global var_volt_ac,var_current_ac
     while not is_shutdown:
-        [ac_current, ac_voltage] = gather_data()
+        [ac_voltage, ac_current] = gather_data()
         var_volt_ac = ac_voltage
         var_current_ac = ac_current
+        time.sleep(0.5)
+
+def display_loop():
+    while not is_shutdown:
+        oled_disp()
+        time.sleep(2)
 
 gather_thread = Thread(target=gather_loop)
-display_thread = Thread(target=oled_disp)
+display_thread = Thread(target=display_loop)
 
 
 #signal handling service
@@ -172,14 +180,20 @@ def sensorLive():
     def generate_random_data():
         with app.app_context(): 
             global data,index_data, var_current_ac
-            newdate = ""
-            newCurrent = 0.0
-            if(index_data < n_lines):
-                newdate =  data[int(index_data)][0] + " " + data[int(index_data)][1]
-                newCurrent = data[int(index_data)][4]
-            if(index_data >= n_lines):
-                newdate = datetime.datetime.now()
-                newCurrent = var_current_ac
+            #[newVolt,newCurrent]=gather_data()
+            #newdate = ""
+            #newCurrent = 0.0
+            newCurrent = var_current_ac
+            newdate = datetime.datetime.now()
+            #newCurrent = var_current_ac
+            #print(var_current_ac)
+            #if(index_data < n_lines):
+            #    newdate =  data[int(index_data)][0] + " " + data[int(index_data)][1]
+            #    newCurrent = data[int(index_data)][4]
+            #if(index_data >= n_lines):
+            #    newdate = datetime.datetime.now()
+            #    newCurrent = var_current_ac
+            #oled_disp()
             index_data=int(index_data)+1
             json_data = json.dumps({'date': newdate, 'current': newCurrent, 'reset':reset}, default=str)
             yield f"data:{json_data}\n\n"
@@ -190,11 +204,11 @@ def sensorLive():
 
 if __name__ == '__main__':
    
-    #gather_thread.start()
-    #display_thread.start()
+    gather_thread.start()
+    display_thread.start()
     
     app.run(debug=True, threaded=True, host='0.0.0.0', port=5000)
-    gather_data()
+    #gather_loop()
     print(f"END at {datetime.datetime.now()}")
     #gather_thread.join()
     #ac_tel_file.close()
