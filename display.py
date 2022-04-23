7cca6c3f1e04622df4e2b0c40e794c1a214a67d6
javier pernia <javierpernia89@gmail.com>
2022-04-22 20:28:12 -0500

    last valid display driver

--- display.py
+++ display.py
@@ -1,12 +1,8 @@
 #!/usr/local/bin/python3.8
 #review https://docs.micropython.org/en/latest/esp8266/tutorial/ssd1306.html
-
-#https://learn.adafruit.com/circuitpython-libraries-on-linux-and-the-nvidia-jetson-nano?view=all
 import time
 
-import board
-import busio
-import adafruit_ssd1306
+import Adafruit_SSD1306
 
 from PIL import Image
 from PIL import ImageDraw
@@ -34,43 +30,39 @@ var_volt_ac = ""
 var_date = ""
 var_time = ""
 
-WIDTH = 128
-HEIGHT = 64  # Change to 64 if needed
-BORDER = 5
+
 # 128x32 display with hardware I2C:
-i2c = busio.I2C(board.SCL_1, board.SDA_1)
-oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3c)
-# Clear display.
-oled.fill(0)
-oled.show()
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=None, i2c_bus=0, gpio=1) # setting gpio to 1 is hack to avoid platform detection
 
+# Initialize library.
+disp.begin()
 
+    # Clear display.
+disp.clear()
+disp.display()
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-image = Image.new("1", (oled.width, oled.height))
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-image = Image.new("1", (oled.width, oled.height))
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
 
-# Get drawing object to draw on image.
+    # Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
 
-# Draw a white background
-draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
+    # Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-# Draw a smaller inner rectangle
-draw.rectangle(
-    (BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1),
-    outline=0,
-    fill=0,
-)
+    # Draw some shapes.
+    # First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+    # Move left to right keeping track of the current x position for drawing shapes.
+x = 0
 
-# Load default font.
+    # Load default font.
 font = ImageFont.load_default()
-
-
-
 
 print("before while")
 lines_before = 0
@@ -97,7 +89,7 @@ while True:
     var_volt_ac = line[2]
     
             # Draw a black filled box to clear the image.
-   
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
 
             # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
     cmd = "ifconfig wlan0 | grep 'inet ' | cut -c 14-26"
@@ -114,35 +106,24 @@ while True:
     Date = subprocess.check_output(cmd, shell = True )
 
             # Write two lines of text.
-    (font_width, font_height) = font.getsize(IP)
-    draw.text((oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2 ),       "IP: " + IP.decode('utf-8'),  font=font, fill=255)
-    (font_width, font_height) = font.getsize(var_date)
-    draw.text((oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2 +8),   "DATE: " + var_date,  font=font, fill=255)
-    (font_width, font_height) = font.getsize(var_time)
-    draw.text((oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2 +16),   "TIME: " + var_time,  font=font, fill=255)
-    (font_width, font_height) = font.getsize(var_current_ac)
-    draw.text((oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2 +24),  "AMP: " +  var_current_ac + " A",  font=font, fill=255)
-    (font_width, font_height) = font.getsize(var_volt_ac)
-    draw.text((oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2 +32),  "VOLT: " +  var_volt_ac + " V",  font=font, fill=255)
-    (font_width, font_height) = font.getsize(CPU)
-    draw.text((oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2 +40),     CPU.decode('utf-8'), font=font, fill=255)
-    (font_width, font_height) = font.getsize(Disk)
-    draw.text((oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2 +48),  "DISK: " +  Disk.decode('utf-8') ,  font=font, fill=255)
+    draw.text((x, top),       "IP: " + IP.decode('utf-8'),  font=font, fill=255)
+    draw.text((x, top+8),   "DATE: " + var_date,  font=font, fill=255)
+    draw.text((x, top+16),   "TIME: " + var_time,  font=font, fill=255)
+    draw.text((x, top+24),  "AMP: " +  var_current_ac + " A",  font=font, fill=255)
+    draw.text((x, top+32),  "VOLT: " +  var_volt_ac + " V",  font=font, fill=255)
+    draw.text((x, top+40),     CPU.decode('utf-8'), font=font, fill=255)
+    draw.text((x, top+48),  "DISK: " +  Disk.decode('utf-8') ,  font=font, fill=255)
     # Display image.
-    # Display image
-    oled.image(image)
-    
+    disp.image(image)
     end = time.time()
-    oled.show()
-    '''
     if(end-start >5):
         print("time elapsed")
         start = end
         try:
-            oled.show()
+            disp.display()
         except:
             print(f"ERROR display 0x3C i2c disconnection")
 
-    '''
+    
         
 s.close()
