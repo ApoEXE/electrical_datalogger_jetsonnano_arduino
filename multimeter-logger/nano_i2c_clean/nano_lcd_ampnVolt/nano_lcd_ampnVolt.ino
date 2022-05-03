cd87d6d29ced126efddd219f4821639a74f0a856
javier pernia <javierpernia@gmail.com>
2022-05-02 23:07:48 -0500

    added voltage for panel and relay control

--- multimeter-logger/nano_i2c_clean/nano_lcd_ampnVolt/nano_lcd_ampnVolt.ino
+++ multimeter-logger/nano_i2c_clean/nano_lcd_ampnVolt/nano_lcd_ampnVolt.ino
@@ -16,15 +16,22 @@ ACS712  ACS_2(A2, 5.0, 1023, 66);
 //digital read
 int ac_curr_dig = 0;//save the digital conversion of current
 int ac_volt_dig = 0;//save the digital conversion of voltage
+
+int ac_curr_dig_2 = 0;//save the digital conversion of current
+int ac_volt_dig_2 = 0;//save the digital conversion of voltage
+
 unsigned int sample = 50;//number of samples to take before sending to lcd
 volatile bool flag1 = false; //send on received command from jetson
-
+const int relay = 11;
+const int led = 13;
+volatile bool flag2  = false;
+int led_state = LOW;
 
 void setup() {
   // set up the LCD's number of columns and rows:
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  pinMode(11, OUTPUT);
+  pinMode(led, OUTPUT);
+  pinMode(relay, OUTPUT);
   ACS.autoMidPoint();
   ACS_2.autoMidPoint();
   //ACS.setNoisemV(70);
@@ -33,7 +40,7 @@ void setup() {
   Wire.onRequest(sendEvent);
 
   Serial.print("sin miedo al exito");
-  digitalWrite(13, HIGH);
+  digitalWrite(led, led_state);
 }
 
 void loop() {
@@ -47,26 +54,38 @@ void loop() {
   }
  
   double dig_mA = 0;
+  double dig_mA_2 = 0;
   for(int i = 0; i< sample; i++)
       dig_mA += ACS.mA_AC(60);
+      dig_mA_2 += ACS_2.mA_AC(60);
   int mA = (dig_mA/sample);
+  int mA_2 = (dig_mA_2/sample);
   ac_curr_dig = mA;
+  ac_curr_dig_2 = mA_2;
   float AC_amp = mA/1000.0;
   if(AC_amp < 0.07){
     AC_amp = 0;
     ac_curr_dig=0;
+  }
+  float AC_amp_2 = mA_2/1000.0;
+    if(AC_amp_2  < 0.07){
+    AC_amp_2  = 0;
+    ac_curr_dig_2=0;
   }
   //Serial.print("AC_amp ");
   //Serial.print(AC_amp);
   //Serial.print(" ");
 
   double dig_volt = 0;
+  double dig_volt_2 = 0;
   for(int i = 0; i< sample; i++){
       dig_volt += analogRead(A1);
+      dig_volt_2 += analogRead(A3);
   }
   int anaVolt = (dig_volt/sample);
   ac_volt_dig = anaVolt;
-  
+  int anaVolt_2 = (dig_volt_2/sample);
+  ac_volt_dig_2 = anaVolt_2;
   //float volt_div = anaVolt * 5.1/1023.0;
   //float volt_in = volt_div*(1000+880000)/1000;
   //float volt_ac = (volt_in/sqrt(2)) + 5;
@@ -78,10 +97,24 @@ void loop() {
 
 void receiveEvent(int hoeMny)
 {
+  led_state = !led_state;
+  digitalWrite(relay, led_state);
   byte cmd = Wire.read();  //read the received byte
   if (cmd == 0x0A)    //be sure that 0x0A is coming from MEGA
   {
     flag1  = true;
+    
+  }
+  if (cmd == 0x0B)    //relay toggle
+  {
+    if(flag2==false){
+    flag2  = true;
+    digitalWrite(relay, HIGH);
+    }
+    else{
+    flag2  = false;
+    digitalWrite(relay, LOW);
+    }
     
   }
 }
@@ -89,9 +122,12 @@ void receiveEvent(int hoeMny)
 void sendEvent()
 {
   Serial.println("entre en sendEvent");
-
-  Wire.write(ac_curr_dig>>8&0xff);
-  Wire.write(ac_curr_dig);
-  Wire.write(ac_volt_dig>>8&0xff);
-  Wire.write(ac_volt_dig);
+  Wire.write(ac_curr_dig>>8&0xff);//byte 1
+  Wire.write(ac_curr_dig);//byte 2
+  Wire.write(ac_volt_dig>>8&0xff);//byte 3
+  Wire.write(ac_volt_dig);//byte 4
+  Wire.write(ac_curr_dig_2>>8&0xff);//byte 5
+  Wire.write(ac_curr_dig_2);//byte 6
+  Wire.write(ac_volt_dig_2>>8&0xff);//byte 7
+  Wire.write(ac_volt_dig_2);//byte 8
 }
